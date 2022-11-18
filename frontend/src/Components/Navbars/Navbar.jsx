72126becf820ccad8a0f6be168ec1bd66e3643c0
jeremyytann <tanchiachun2021@gmail.com>
2022-11-18 23:42:47 +0800

    completed part of the frontend

---
+++ frontend/src/Components/Navbars/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from '@mui/material';
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMode, setLogout } from 'States';
+import { useNavigate } from 'react-router-dom';
+import FlexBetween from 'Components/FlexBetween';
+
+const Navbar = () => {
+    const [isMobileMenuTOggled, setIsMobileMenuToggled] = useState(false);
+
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
