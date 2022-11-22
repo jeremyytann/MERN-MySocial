49100471b7d35a9ddb891331c1c1537e6525d00e
jeremyytann <tanchiachun2021@gmail.com>
2022-11-22 13:06:25 +0800

    complete profile

--- frontend/src/Components/Friend.jsx
+++ frontend/src/Components/Friend.jsx
@@ -61,14 +61,17 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
                 </Box>
             </FlexBetween>
 
-            <IconButton
-                onClick={() => patchFriend()}
-                sx={{ backgroundColor: primaryLight, p: "0.6rem" }}>
-                {isFriend ? 
-                    <PersonRemoveOutlined sx={{ color: primaryDark }} /> :
-                    <PersonAddOutlined sx={{ color: primaryDark }} />
-                }
-            </IconButton>
+            {_id !== friendId ?
+                <IconButton
+                    onClick={() => patchFriend()}
+                    sx={{ backgroundColor: primaryLight, p: "0.6rem" }}>
+                    {isFriend ? 
+                        <PersonRemoveOutlined sx={{ color: primaryDark }} /> :
+                        <PersonAddOutlined sx={{ color: primaryDark }} />
+                    }
+                </IconButton> : null
+            }
+            
         </FlexBetween>
     )
 }
